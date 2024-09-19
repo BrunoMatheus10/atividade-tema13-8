315e0751f2154052903f2e0bcfd63d5b3e00b6a3
BrunoMatheus10 <brunomatheus88300432@gmail.com>
2024-09-18 23:04:43 -0300

    Add files via upload

---
+++ atividade-tema13-8.py
@@ -0,0 +1,10 @@
+# Recebe a entrada do usuário
+campo = input("Digite algo: ")
+
+# Verifica se o campo está vazio
+if campo == "":
+    # Se a string estiver vazia, imprime que o campo está vazio
+    print("O campo está vazio")
+else:
+    # Se a string não estiver vazia, imprime que o campo não está vazio
+    print("O campo não está vazio")
